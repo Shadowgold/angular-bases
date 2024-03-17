1177a6bdd777038910980050833e46a138904fee
Agustin Espinoza <agustin.espinoza.21@gmail.com>
2024-03-17 20:38:36 -0300

    Input and Output events

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  @Input()
+  public characterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  // onDelete = index
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void {
+    //TODO Emitir el ID del personaje
+    this.onDelete.emit(index);
+    console.log(index);
+  }
 
 }
+
+
